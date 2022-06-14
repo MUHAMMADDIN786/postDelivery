cf9e7d5d340ffee4f18679cbfa5f175d7e892eb9
MUHAMMADDIN <azzadaamir786@gmail.com>
2022-06-14 18:49:34 +0500

    first Commit

---
+++ src/main/java/com/mytech/postdelivery/PostdeliveryApplication.java
@@ -0,0 +1,32 @@
+package com.mytech.postdelivery;
+
+import com.mytech.postdelivery.model.Rider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@SpringBootApplication
+public class PostdeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostdeliveryApplication.class, args);
+    }
+
+//    @Bean
+//    public FilterRegistrationBean corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+//        bean.setOrder(1);
+//        return bean;
+//    }
+}
